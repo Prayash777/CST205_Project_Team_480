a852439ba01cc27508d88e8039518cb1430e4e13
Prayash Raj Singh <prayashsingh701@gmail.com>
2024-07-21 06:48:49 -0700

    Created four html pages for the users to pick the images and the functionality to keep track of the type of design they like

--- project.py
+++ project.py
@@ -4,12 +4,44 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from datetime import datetime
+from function import maxIndex
 index_count = [0,0,0,0]
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap5(app)
-
-
 @app.route('/', methods=('GET', 'POST'))
 def index():
     return render_template('index.html')
+@app.route('/<title><page>')
+def index1(title, page):
+    print (title, page)
+    print (index_count)
+    if title == "cyber":
+        index_count[0]+=1
+    if(title == "earthy"):
+        index_count[1]+=1
+    if(title == "grunge"):
+        index_count[2]+=1
+    if(title == "minimalist"):
+        index_count[3]+=1
+
+    if(page == '1'):
+        return render_template('index2.html')
+    if(page == '2'):
+        return render_template('index3.html')
+    if(page == '3'):
+        return render_template('index4.html')
+    if(page == '4'):
+        index = maxIndex(index_count)
+        print(index)
+    if(index == 1):
+        return render_template('cyber.html')
+    elif(index == 4):
+        return render_template('minimalism.html')
+    else:
+        render_template('index.html')
+
+
+
+print(index_count)
+    
