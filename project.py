bb6cec6e0f2223c534067e66a4ee80da8f99bdf3
Prayash Raj Singh <114454669+Prayash777@users.noreply.github.com>
2024-07-25 00:33:08 -0700

    Update project.py
    Comments added

--- project.py
+++ project.py
@@ -4,16 +4,22 @@ from flask import Flask, render_template, flash, redirect
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from function import maxIndex
+#index to count the clicks on the images.
 index_count = [0,0,0,0]
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap5(app)
+
+#Displays the first page of the application. -Prayash Raj Singh.
 @app.route('/', methods=('GET', 'POST'))
 def index():
     return render_template('index.html')
-
+#Gets the title and page from the html and keeps track of the number of clicks made on 
+#the specific image type. Depending on the previous page number it will display the upcoming page. And while
+#at the end of the page, it will see the highest number of index count and displays the information for that
+#image type. - Prayash Raj Singh.
 @app.route('/<title><page>')
 def index1(title, page):
     print (title, page)
